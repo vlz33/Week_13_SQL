25bb3aa26972b6cb7f2ec336c37c3883d14b2e07
Ville Toomik <vtoomik@tktk.ee>
2022-03-06 14:36:01 +0200

    new projects

---
+++ SQLiteCountries/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SQLite;
+
+
+namespace SQLiteCountries
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database() 
+        {
+            myConnection = new SQLiteConnection("Data Source = countrycapital.db");
+        }
+
+        public void OpenConnection() 
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection() 
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries () 
+        {
+            Database databaseObj = new Database();
+            string query = "select Country.countryName as Country, Capital.capitalName AS Capital from Capital join Country on Capital.countryId = Country.rowid";
+
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country:{data["Country"]}, Capital:{data["Capital"]}");
+                }
+            }
+
+            databaseObj.CloseConnection();
+        }
+    }
+}
